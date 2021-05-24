57cbf2fdd6562927c8cad87f5026adc4efd77ccf
MELKI Lazhari <maroua.melki7@etu.univ-lorraine.f>
2021-05-24 13:56:37 +0200

    methode dans modele commentaire.php

--- src/modele/Commentaire.php
+++ src/modele/Commentaire.php
@@ -7,9 +7,12 @@ class Commentaire extends \Illuminate\Database\Eloquent\Model {
     protected $primaryKey = 'ID';
     public $timestamps = false;
 
-   public static function createNewCo(){
-   	
-   }
+   public static function createNewCo($newCom){
+   	$commentaire = new Commentaire();
+   	$commentaire->titre = $newCom['titre'];
+   	$commentaire->texte = $newCom['texte'];
+   	$commentaire->id_utilisateur = $newCom['id_utilisateur'];
+   	$commentaire->save();
 }
 
 ?>
